9d8ce5b43c7a295f513ab30eb7f3e21ca4701a65
Maxwell DeMaio <maxdman321@gmail.com>
2020-05-06 22:52:11 -0400

    Began process of importing books.csv into the cloud db

--- import.py
+++ import.py
@@ -1,1 +1,10 @@
 # Insertion of book CSV data into the database
+import csv
+
+with open("books.csv") as csvFile:
+    reader = csv.reader(csvFile)
+    keys = next(reader)[:]
+    books = list(reader)
+
+print(keys)
+print(books[0])
