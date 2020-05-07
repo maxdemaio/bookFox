9160e70b66b8c6f83724059261052bccff4c5299
Maxwell DeMaio <maxdman321@gmail.com>
2020-05-06 22:51:40 -0400

    Created routes and updated methods

--- application.py
+++ application.py
@@ -1,9 +1,11 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, redirect, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+from helpers import login_required
 
 app = Flask(__name__)
 
@@ -21,15 +23,77 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    # Check to see if this makes them have to log back in
+    # After visiting home page
+    # # Forget user_id
+    # session.clear()
+
+    # User reached route via POST (as by submitting login via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("user"):
+            msg = "Please provide a username"
+            return render_template("apology.html", msg=msg)
+
+        # Ensure password was submitted
+        elif not request.form.get("pass"):
+            msg = "Please provide a password"
+            return render_template("apology.html", msg=msg)
+
+        # TODO
+        # # Query database for username
+        # rows = db.execute("SELECT * FROM users WHERE username = :username",
+        #                   username=request.form.get("username"))
+
+        # # Ensure username exists and password is correct
+        # if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        #     return apology("invalid username and/or password", 403)
+
+        # # Remember which user has logged in
+        # session["user_id"] = rows[0]["id"]
+
+        # Redirect user to search page
+        return redirect(url_for("search"))
+
+    else:
+        return render_template("index.html")
 
 @app.route("/register")
 def register():
     return render_template("register.html")
 
-
+# Accept get and post methods. Post for login.
 @app.route("/search")
+@login_required
 def search():
     return render_template("search.html")
+
+# Change to a post only route
+@app.route("/results")
+def results():
+
+    # TODO
+    # Query db based on search selection
+    # get id of books as well (important for when we post to books/id#)
+    # Display a table of information based on the user's search
+    
+    # Limit to 40 results (disclaim this as well)
+    # When the user searches by year, order alphabetically by title
+    # Otherwise, order from newest to oldest
+
+
+    return render_template("results.html")
+
+# Have this route accept id argument from result page
+@app.route("/books/<int:id>")
+def books(id):
+
+    # TODO
+    # Contact the API using the isbn (retrieved by using id)
+
+
+    # Render the books bage with all Goodreads info, and info from our website
+    return render_template("books.html", id=id)
